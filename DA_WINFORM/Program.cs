4fdd9121a35b6f0c7dec92b7df44f1b58b77f678
TimmyVonAtaraxia <timmyvonataraxia@gmail.com>
2025-03-25 16:30:18 +0700

    frm Login/ SignIn

--- DA_WINFORM/Program.cs
+++ DA_WINFORM/Program.cs
@@ -16,7 +16,7 @@ namespace DA_WINFORM
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmLogin());
         }
     }
 }
---
+++ DA_WINFORM/frmLogin.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DA_WINFORM
+{
+    public partial class frmLogin : Form
+    {
+        User_BLL bllUser;
+        public frmLogin()
+        {
+            InitializeComponent();
+            bllUser = new User_BLL();
+        }
+
+        void CheckBox()
+        {
+            tbl_User User = new tbl_User();
+            User.UserName = txtTenDN.Text;
+            User.Password = txtMK.Text;
+            //string quyen = "";
+            if (bllUser.PhQuyen(User) == "Admin")
+            {
+                Form1 f1 = new Form1();
+                f1.Show();
+            }
+
+            if (bllUser.PhQuyen(User) == "Employee")
+            {
+      
+            }
+            if (bllUser.PhQuyen(User) == "Guest")
+            {
+         
+            }
+            if (bllUser.PhQuyen(User) == "Fail")
+            {
+                MessageBox.Show("Sai tên đăng nhập hoặc mật khẩu!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+     
+       
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            txtMK.PasswordChar = '\u25CF';
+        }
+
+        private void guna2GradientPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void cbShowMK_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbShowMK.Checked)
+            {
+                txtMK.PasswordChar = '\0';
+            }
+
+            else
+                txtMK.PasswordChar = '\u25CF';
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            CheckBox();
+        }
+
+        private void btnSingIn_Click(object sender, EventArgs e)
+        {
+            frmSignIn signInForm = new frmSignIn();
+            signInForm.ShowDialog();
+        }
+
+        private void ControlMin_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
